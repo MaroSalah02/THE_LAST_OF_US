8f8ac4ee1ff6234c6b62960de604f09a60cb590e
RiadOmar <126183421+RiadOmar@users.noreply.github.com>
2023-03-17 18:38:37 +0200

    Implemented Character Class

--- src/model/characters/Character.java
+++ src/model/characters/Character.java
@@ -1,5 +1,57 @@
 package model.characters;
+import java.awt.Point;
 
-public class Character {
+abstract public class Character {
 
+	private String name;
+	private Point location;
+	private int maxHp;
+	private int currentHp;
+	private int attackDmg;
+	private Character target;
+	
+	public Character(String name, int maxHp, int attackDmg) {
+		this.name = name;
+		this.maxHp = maxHp;
+		this.attackDmg = attackDmg;
+	}
+
+	public Point getLocation() {
+		return location;
+	}
+
+	public void setLocation(Point location) {
+		this.location = location;
+	}
+
+	public int getCurrentHp() {
+		return currentHp;
+	}
+
+	public void setCurrentHp(int currentHp) {
+		this.currentHp = currentHp;
+	}
+
+	public Character getTarget() {
+		return target;
+	}
+
+	public void setTarget(Character target) {
+		this.target = target;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getMaxHp() {
+		return maxHp;
+	}
+
+	public int getAttackDmg() {
+		return attackDmg;
+	}
+	
+	
+	
 }
