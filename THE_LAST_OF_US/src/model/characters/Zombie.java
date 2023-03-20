15a8ab91116c620e1618c2be355d9e72d5661bad
RiadOmar <126183421+RiadOmar@users.noreply.github.com>
2023-03-20 14:21:08 +0200

    ZOMBIE_COUNT->private & added setter and getter

--- THE_LAST_OF_US/src/model/characters/Zombie.java
+++ THE_LAST_OF_US/src/model/characters/Zombie.java
@@ -1,11 +1,19 @@
 package model.characters;
 
 public class Zombie extends Character {
-	static int ZOMBIES_COUNT = 0;
+	private static int ZOMBIES_COUNT = 0;
 	
 	public Zombie()
 	{
 		super("Zombie " + (++ZOMBIES_COUNT), 40, 10);	
+	}
+
+	public static int getZOMBIES_COUNT() {
+		return ZOMBIES_COUNT;
+	}
+
+	public static void setZOMBIES_COUNT(int zOMBIES_COUNT) {
+		ZOMBIES_COUNT = zOMBIES_COUNT;
 	}
 	
 }
