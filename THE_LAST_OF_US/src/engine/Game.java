f7f322747232bf002e2342fb9d5856842057e08c
Marawan Salah <Marawansalah002@hotmail.com>
2023-05-06 21:34:25 +0200

    Adding StartGame (Partially) method, CheckWin,checkGameOver and endTurn

--- THE_LAST_OF_US/src/engine/Game.java
+++ THE_LAST_OF_US/src/engine/Game.java
@@ -4,10 +4,10 @@ package engine;
 import java.awt.Point;
 import java.io.*;
 import java.util.ArrayList;
-
+import java.util.*;
 import model.characters.*;
-import model.world.Cell;
 import model.collectibles.*;
+import model.world.*;
 
 
 public class Game {
@@ -62,6 +62,44 @@ public class Game {
 			(map[X][Y+1]).setVisible(true);
 		
 	}
+	public static void startGame(Hero h) {
+		// spawn the necessary Collectibles
+		ArrayList<Supply> SupArray = h.getSupplyInventory();
+		ArrayList<Vaccine> vaccinoArray =  h.getVaccineInventory();
+		for(int i =0; i<5; i++) {
+			SupArray.add(new Supply());
+			vaccinoArray.add(new Vaccine());
+		}
+		for(int x =0;x<14; x++) {
+			for(int y =0; y<14; y++) {
+				map[x][y] = null;
+			}
+		}
+		// spawn 5 traps randomly around the map
+		int x;
+		int y;
+		for(int i = 0; i<5; i++) {
+			do {
+				x = ((int)(Math.random()*15));
+				y = ((int)(Math.random()*15));
+			}while(map[x][y] != null);
+			map[x][y] = new TrapCell();
+		}
+		// To do : spawn 10 zombies randomly around the map
+		// To do : add the hero to the controllable heroes pool and removing from the availableHeroes
+		// To do : finally allocating the hero to the bottom left corner of the map.
+		
+		
+	}
 	
+//	public static boolean checkWin() {
+//		// To do : This method checks the win conditions for the game.
+//	}
+//	public static boolean checkGameOver() {
+//		// To do : This method checks the conditions for the game to end 
+//	}
+	public static void endTurn() {
+		
+	}
 }
 
