694f0141abfaccc027460ea468f56afbfdac411c
RiadOmar <126183421+RiadOmar@users.noreply.github.com>
2023-05-08 19:46:04 +0200

    merge

--- THE_LAST_OF_US/src/model/characters/Character.java
+++ THE_LAST_OF_US/src/model/characters/Character.java
@@ -3,6 +3,7 @@ import java.awt.Point;
 import java.util.ArrayList;
 import engine.Game;
 import exceptions.*;
+import model.world.*;
 abstract public class Character {
 
 	private String name;
@@ -75,22 +76,13 @@ abstract public class Character {
 				int j=Game.zombies.indexOf(z);
 				Game.zombies.remove(j);
 			}
-<<<<<<< HEAD
-		}		
-		Point L =this.getLocation();
-		Game.map[14-L.y][L.x]=null;
-=======
+
+		}
+
 			Point L =this.getLocation();
 			Game.map[(int)L.getX()][(int)L.getY()]= new CharacterCell(null);
 		}		
 		
->>>>>>> refs/remotes/origin/main
-		}
-<<<<<<< HEAD
-=======
-		
-	
->>>>>>> refs/remotes/origin/main
 	
 	
 	public boolean adjacentTarget() {
